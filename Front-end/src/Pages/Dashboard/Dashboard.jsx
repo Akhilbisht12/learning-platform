e1ad34b3768a5c002a93cff03283b96db706230c
Akhilbisht12 <akhilbisht83@gmail.com>
2022-01-19 11:34:09 +0530

    shifted to email verification

--- Front-end/src/Pages/Dashboard/Dashboard.jsx
+++ Front-end/src/Pages/Dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ import BottomTabs from "../../components/Layout/BottomTabs";
 import Navbar from "../../components/UI/Navigation/Navbar/Navbar";
 import Layout from "../../components/Layout/Layout";
 import authService from "../../ApiServices/auth.service";
+import Loader from "react-loader-spinner";
 
 ChartJS.register(
   CategoryScale,
@@ -25,94 +26,104 @@ ChartJS.register(
   ArcElement
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "bottom",
-    },
-    title: {
-      display: true,
-      text: "Overview",
-    },
-  },
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-      },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-      },
-    ],
-  },
-};
-
-const labels = ["Overview"];
-
-
-
-const pieOptions = {
-  plugins: {
-    legend: {
-      position: "bottom",
-    },
-  },
-};
-
 const Dashboard = () => {
-  const [coursesWatched, setcoursesWatched] = useState(0);
+  const [coursesWatched, setcoursesWatched] = useState();
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const user = localStorage.getItem("userId");
-    authService.GetCourseCount(user).then((response) => {
-      setcoursesWatched(response.data.count);
-      console.log(response.data.count)
-    });
+    getData();
   }, []);
+  const getData = async () => {
+    const userId = localStorage.getItem("userId");
+    await authService.GetCourseCount(userId).then((response) => {
+      setcoursesWatched(response.data.count);
+      console.log(response.data.count);
+    });
+    await authService.GetUser(userId).then((response) => {
+      setUser(response.data.user);
+    });
+    setLoading(false);
+  };
+  let data = null;
+  let pieData = null;
+  if (user) {
+    const labels = ["Overview"];
+    data = {
+      labels,
+      datasets: [
+        {
+          label: "Modules com.",
+          data: labels.map(() => coursesWatched),
+          backgroundColor: "purple",
+        },
+        {
+          label: "Villages Vis.",
+          data: labels.map(() => user.swot.length),
+          backgroundColor: "blue",
+        },
+        {
+          label: "Surverys Cond.",
+          data: labels.map(() => user.swot.length + user.influencers.length),
+          backgroundColor: "violet",
+        },
+        {
+          label: "Influencers data",
+          data: labels.map(() => user.influencers.length),
+          backgroundColor: "pink",
+        },
+      ],
+    };
+    pieData = {
+      labels: ["Influencer", "Participants", "SWOT", "Modules"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [
+            user.influencers.length,
+            user.influencers.length,
+            user.swot.length,
+            coursesWatched,
+          ],
+          backgroundColor: ["purple", "blue", "violet", "pink"],
+          borderColor: ["white", "white", "white", "white"],
+          borderWidth: 2,
+        },
+      ],
+    };
+  }
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Overview",
+      },
+    },
+  };
 
-   let data = {
-    labels,
-    datasets: [
-      {
-        label: "Modules com.",
-        data: coursesWatched,
-        backgroundColor: "purple",
+  const pieOptions = {
+    plugins: {
+      legend: {
+        position: "bottom",
       },
-      {
-        label: "Villages Vis.",
-        data: labels.map(() => 20),
-        backgroundColor: "blue",
-      },
-      {
-        label: "Surverys Cond.",
-        data: labels.map(() => 5),
-        backgroundColor: "violet",
-      },
-      {
-        label: "Influencers data",
-        data: labels.map(() => 100),
-        backgroundColor: "pink",
-      },
-    ],
+    },
   };
-  const pieData = {
-    labels: ["Influencer", "Participants", "SWOT", "Modules"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, coursesWatched],
-        backgroundColor: ["purple", "blue", "violet", "pink"],
-        borderColor: ["white", "white", "white", "white"],
-        borderWidth: 2,
-      },
-    ],
-  };
+  if (loading) {
+    return (
+      <Layout>
+        <Loader
+          type="Puff"
+          color="#08BD80"
+          height={50}
+          width={50}
+          className="loader"
+        />
+      </Layout>
+    );
+  }
   return (
     <Layout>
       <Bar height="250px" options={options} data={data} />
@@ -136,7 +147,7 @@ const Dashboard = () => {
             ></div>
             <div>Village Visited</div>
           </div>
-          <div>6</div>
+          <div>{user.swot.length}</div>
         </div>
         <div className="d-flex justify-content-between px-3 my-2">
           <div className="d-flex justify-content-start align-items-center">
@@ -146,14 +157,14 @@ const Dashboard = () => {
             ></div>
             <div>Surveys Conducted</div>
           </div>
-          <div>6</div>
+          <div>{user.swot.length + user.influencers.length}</div>
         </div>
         <div className="d-flex justify-content-between px-3 my-2">
           <div className="d-flex justify-content-start align-items-center">
             <div className="p-2 m-1" style={{ backgroundColor: "pink" }}></div>
             <div>Influencer Data</div>
           </div>
-          <div>6</div>
+          <div>{user.influencers.length}</div>
         </div>
       </div>
       {/* pie chart */}
@@ -170,7 +181,7 @@ const Dashboard = () => {
             ></div>
             <div>Influencers</div>
           </div>
-          <div>{coursesWatched}</div>
+          <div>{user.influencers.length}</div>
         </div>
         <div className="d-flex justify-content-between px-3 my-2">
           <div className="d-flex justify-content-start align-items-center">
@@ -180,7 +191,7 @@ const Dashboard = () => {
             ></div>
             <div>Participants</div>
           </div>
-          <div>6</div>
+          <div>{user.influencers.length}</div>
         </div>
         <div className="d-flex justify-content-between px-3 my-2">
           <div className="d-flex justify-content-start align-items-center">
@@ -190,7 +201,7 @@ const Dashboard = () => {
             ></div>
             <div>Swots</div>
           </div>
-          <div>6</div>
+          <div>{user.swot.length}</div>
         </div>
         <div className="d-flex justify-content-between px-3 my-2 mb-5">
           <div className="d-flex justify-content-start align-items-center">
